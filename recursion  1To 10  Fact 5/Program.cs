09d9e943e4fed5772f9995cf7deee5bc5b3daa3c
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-08-18 10:27:44 +0530

    Add project files.

---
+++ recursion  1To 10  Fact 5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace recursion__1To_10__Fact_5
+{
+
+
+
+    class Program
+    {
+        //  1 to 10 number Print :
+        int num = 1;
+        void p()
+        {
+            if (num == 11)
+            {
+                return;
+            }
+            Console.WriteLine(num);
+            num++;
+            p();
+        }
+
+        // factorial 5 
+        int n = 5;
+        int result = 1;
+        int fact()
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            result = result * n;
+            n--;
+            fact();
+            return result;
+        }
+
+
+        public static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.p();
+            int f = obj.fact();
+
+            Console.WriteLine("fact :" + f);
+        }
+
+    }
+}
